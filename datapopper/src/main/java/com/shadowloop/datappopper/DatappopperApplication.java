3ec6669c18873fb43bf86b4e54fc79b0d697b0b9
kyr7 <kyr7@>
2025-05-09 09:35:35 +0200

    init

---
+++ datapopper/src/main/java/com/shadowloop/datappopper/DatappopperApplication.java
@@ -0,0 +1,13 @@
+package com.shadowloop.datappopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatappopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatappopperApplication.class, args);
+	}
+
+}
