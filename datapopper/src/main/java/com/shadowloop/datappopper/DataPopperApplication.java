c6c2610f006371849ac7c0141c3b4e6650fd859a
kyr7 <kyr7@>
2025-05-09 12:48:59 +0200

    init reader

--- datapopper/src/main/java/com/shadowloop/datappopper/DataPopperApplication.java
+++ datapopper/src/main/java/com/shadowloop/datappopper/DataPopperApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatappopperApplication {
+public class DataPopperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DatappopperApplication.class, args);
+		SpringApplication.run(DataPopperApplication.class, args);
 	}
 
 }
